fbb04d6ac79dc2cced89d3be7c3e1e54694a69d0
21-UR-0241 <parinasrenold026@gmail.com>
2026-02-10 20:05:44 +0800

    Update Aisuggestions.jsx

--- frontend/src/admin/components/Aisuggestions.jsx
+++ frontend/src/admin/components/Aisuggestions.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import '../styles/AISuggestions.css';
+import '../styles/Aisuggestions';
 
 
 function AISuggestions({ conversation, messages, onSelectSuggestion }) {
